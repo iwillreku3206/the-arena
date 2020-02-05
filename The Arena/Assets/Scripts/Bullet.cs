cb0fac1cafb4523b5f751244a765529731507bf6
IWillRekU3206 <iwillreku3206@gmail.com>
2020-02-05 20:07:07 +0800

    Created Unity Project
    Also added: scene, player movement, bullet

---
+++ The Arena/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Player player;
+    public SphereCollider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        collider = gameObject.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * player.bulletSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        System.Console.WriteLine("Collided with {0}", collision.gameObject);
+    }
+}
+    
---
+++ The Arena/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public Object bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))*Time.deltaTime);
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X")));
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, playerTransform.position + (playerTransform.forward * 2) + new Vector3(0, 0.5f), playerTransform.rotation);
+        }
+    }
+
+          
+}
