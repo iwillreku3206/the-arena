134c8641ea9c7f2072ba7deaaf5bee4e6f8f4875
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2020-02-06 14:31:15 +0800

    added player stats in player.cs

--- The Arena/Assets/Scripts/Player.cs
+++ The Arena/Assets/Scripts/Player.cs
@@ -1,9 +1,32 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float bulletSpeed = 5.0f;
+    
+    public int health = 100;
+    public int maxHealth = 100;
+    
+    public int stamina = 100;
+    public int maxStamina = 100;
+    
+    public bool dead = false;
+    
+    void Update() {
+        if (health > maxHealth) {
+            health = maxHealth;
+        }
+        if (0 > health) {
+            dead = true;
+        }
+        if (stamina > maxStamina) {
+            stamina = maxStamina
+        }
+        if (0 > stamina) {
+            stamina = 0
+        }
+    }
 
 }
