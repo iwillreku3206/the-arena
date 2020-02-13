0121962d75fa7ae64489846d985d6b68f6172c1a
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2020-02-13 14:14:45 +0800

    Update Spawner.cs

--- The Arena/Assets/Scripts/Spawner.cs
+++ The Arena/Assets/Scripts/Spawner.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-
-
+    public object enemy;
+    
+    public Game game;
+    
+    int fixedFramesPerSecond;
+    int sCount = 0;
+    
+    private void FixedUpdate() {
+        fixedFramesPerSecond = 1 / Time.fixedDeltaTime;
+        
+        sCount++;
+        if (game.enemySpawnRate <= sCount) {
+            Instantiate(enemy, gameObject.position, gameObject.rotation);
+            
+            sCount = 0;
+        }
+    }
 }
